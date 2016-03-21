fcedcb3dccb78fa91ed45f1aa3788926ea7d502f
LukeOr98 <luca.or98@hotmail.it>
2016-03-21 22:03:45 +0100

    Correzioni

--- DbConnection/src/it/ecommerceproject/databases/panels/AdminPanel.java
+++ DbConnection/src/it/ecommerceproject/databases/panels/AdminPanel.java
@@ -13,9 +13,10 @@ public class AdminPanel {
         int chosen = 0;
         
         AccountsManager aManager = new AccountsManager();
+        ProductsManager pManager = new ProductsManager();
         
         do {
-            System.out.println("\n1 - Create new user\n2 - Delete a user\n3 - Edit a user\n4 - Print all Users\n5 - Add new product\n6 - Delete a product\n7 - Edit a product\n 8 - Print all products\n9 - Exit\n\n--");
+            System.out.println("\n1 - Create new user\n2 - Delete a user\n3 - Edit a user\n4 - Print all Users\n5 - Add new product\n6 - Delete a product\n7 - Edit a product\n8 - Print all products\n9 - Exit\n\n--");
             
             chosen = input.nextInt();
             
@@ -24,10 +25,10 @@ public class AdminPanel {
                 case 2: if(aManager.deleteUser(u) == 0) {System.out.println("User successfully removed");}break;
                 case 3: if(aManager.editUser()) {System.out.println("User successfully edited");}break;
                 case 4: aManager.printUsers(); break;
-                case 5: break;
-                case 6: break;
-                case 7: break;
-                case 8: break;
+                case 5: if(pManager.addProduct()) {System.out.println("Product successfully added");} break;
+                case 6: if(pManager.deleteProduct() == 0) {System.out.println("Product successfully removed");} break;
+                case 7: if(pManager.editProduct()) {System.out.println("Product successfully edited");}break;
+                case 8: pManager.printProducts();break;
                 case 9: break;
                 default: System.out.println(chosen + ": Unknown command");
             }
--- DbConnection/src/it/ecommerceproject/databases/processes/ProductsManager.java
+++ DbConnection/src/it/ecommerceproject/databases/processes/ProductsManager.java
@@ -1,14 +1,148 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.ecommerceproject.databases.processes;
 
-/**
- *
- * @author Tech
- */
+import it.ecommerceproject.databases.products.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
 public class ProductsManager {
+    
+    ProductsHandler prodHandler = new ProductsHandler();
+    
+    Scanner input = new Scanner(System.in);
+
+    public ProductsManager() {
+        
+    }
+    
+    public boolean addProduct() throws SQLException {
+        
+        prodHandler.open();
+        
+        boolean added = false;
+        String yesNo = "h";
+        boolean found = false;
+        Product p;
+        
+        do{
+            System.out.println("New Product's Name: "); String name = input.next();
+            System.out.println("New Product's Description: "); String desc = input.next();
+            System.out.println("New Product's Price: "); double price = input.nextDouble();
+            List<Product> products = prodHandler.getData();
+            for(int i = 0; i < products.size(); i++){
+                p = products.get(i);
+                if(name.equals(p.getName())){
+                    found = true;
+                }
+            }
+                if(!found){
+                    try {
+                        prodHandler.insert(new Product(name, desc, price));
+                        added = true;
+                    }
+                    catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                    do{
+                        System.out.println("Product successfuly added, do tou want to add another product? [y/n] "); yesNo = input.next();
+                    } while(!((yesNo.equals("y"))||(yesNo.equals("n"))));
+                }
+                else{
+                    do{
+                        System.out.println("Product " + name + " already exists, do you want to retry? [y/n] "); yesNo = input.next();
+                        if(yesNo.equals("y")){
+                            found = false;
+                        }
+                    }while(!((yesNo.equals("y"))||(yesNo.equals("n"))));
+                }
+        }while(yesNo.equals("y"));
+        prodHandler.close();
+        
+        return added;
+    }
+    
+    public int deleteProduct() throws SQLException{
+        
+        //removed = 0 --> rimosso
+        //removed = 1 --> non esiste
+        //removed = 2 --> loggato
+        
+        prodHandler.open();
+        
+        int removed = 2;
+        boolean found = false;
+        int id = 0;
+        Product p;
+        System.out.println("Name: "); String name = input.next();
+        try {
+            List<Product> products = prodHandler.getData();
+            for(int i = 0; i<products.size(); i++){
+                p = products.get(i);
+                if(name.equals(p.getName())){
+                    found = true;
+                    id = p.getID();
+                }
+            }
+            if(!found){
+                removed = 1;
+            }
+                if(found){
+                        prodHandler.delete(id);
+                        removed = 0;
+                    }
+            
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        if(removed == 1){
+            System.out.println("Product " + name + " doesn't exists");
+        }
+        prodHandler.close();
+        return removed;
+    }
+    public boolean editProduct() throws SQLException{
+        prodHandler.open();
+        
+        boolean done = false;
+        Product p;
+        System.out.println("Name: "); String user = input.next();
+        try {
+            List<Product> products = prodHandler.getData();
+            System.out.println("New Name: "); String nname = input.next();
+            System.out.println("New Description: "); String ndesc = input.next();
+            System.out.println("New Price: "); double nprice = input.nextDouble();
+            for(int i = 0; i<products.size(); i++){
+                p = products.get(i);
+                if(!done){
+                    if(user.equals(p.getName())){
+                        p.setName(nname);
+                        p.setDescription(ndesc);
+                        p.setPrice(nprice);
+                        prodHandler.update(p);
+                        done = true;
+                    }
+                }
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        prodHandler.close();
+        return done;
+    }
+    public void printProducts() throws SQLException{
+        prodHandler.open();
+        
+        Product p;
+        
+        List<Product> products = prodHandler.getData();
+        System.out.printf("%5s%20s%20s%20s\n", "ID", "Name", "Description", "Price");
+        for(int i = 0; i<products.size(); i++){
+            p = products.get(i);
+            
+            System.out.printf("%5d%20s%17s...%20.2fEUR\n", p.getID(), p.getName(), p.getDescription(), p.getPrice());
+        }
+    }
     
 }
